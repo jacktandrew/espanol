f15d2adc13c5b1624a04e1f7bd30c0233b646161
jacktandrew <jacktandrew@gmail.com>
2013-01-21 13:39:04 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Espanol::Application.config.secret_token = 'f675d0ed003dbada5a5b6c66c23df752bdae8bd550a402a6228b0f1f4a9e5282c0051e5ab83ed23fd16adcdef9c309b2e9f9822871600a07349f5aa8bdd11545'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Espanol::Application.config.session_store :cookie_store, key: '_Espanol_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Espanol::Application.config.session_store :active_record_store
